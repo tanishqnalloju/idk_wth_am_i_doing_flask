56cf17303df9af9663cc63ea15c195deb59dc262
Venkata Sai Tanishq Nalloju <tanishqnrao@gmail.com>
2020-11-17 22:07:34 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -8,16 +8,16 @@ def index():
 
 @app.route("/merge_sort")
 def merge_sort():
-    return render_template("public/sort.html", Name = "Merge", Function = "Sort", Description ="Merge Sort fasteets sort")
+    return render_template("public/sort.html", Name = "Merge", Job = "Sort", Description ="Merge Sort fasteets sort")
 
 @app.route("/bubble_sort")
 def bubble_sort():
-    return render_template("public/sort.html", Name = "Bubble", Function = "Sort")
+    return render_template("public/sort.html", Name = "Bubble", Job = "Sort")
 
 @app.route("/linear_search")
 def linear_sort():
-    return render_template("public/sort.html", Name = "Linear", Function = "Search")
+    return render_template("public/sort.html", Name = "Linear", Job = "Search")
 
 @app.route("/binary_search")
 def binary_sort():
-    return render_template("public/sort.html", Name = "Binary", Function = "Search")
+    return render_template("public/sort.html", Name = "Binary", Job = "Search")
